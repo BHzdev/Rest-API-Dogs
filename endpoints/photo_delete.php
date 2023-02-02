a5b18bcea4d790e4f374be7797452fe65fffd670
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-02 16:21:05 -0300

    add photo_delete, buscando post pelo id

---
+++ endpoints/photo_delete.php
@@ -0,0 +1,24 @@
+<?php
+// Define a função que lida com os dados da requisição.
+function api_photo_delete($request) {
+  // Busca os dados do usuáiro que está logado.
+  $user = wp_get_current_user();
+  $post_id = $request["id"];
+  // Busca o post pelo id.
+  $post = get_post($post_id);
+  $author_id = (int) $post->post_author;
+  $user_id = (int) $user->ID;
+
+  return rest_ensure_response("Post deletado.");
+}
+
+// Função para registrar o endpoint da API.
+function register_api_photo_delete() {
+  register_rest_route('api', '/photo/(?P<id>[0-9]+)', [
+      'methods' => WP_REST_Server::DELETABLE,
+      'callback' => 'api_photo_delete'
+  ]);
+}
+
+add_action('rest_api_init', 'register_api_photo_delete');
+?>
