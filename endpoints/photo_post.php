9e5ca05a6f91b41eb34b21ea40236b2d80a56683
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-02 15:27:57 -0300

    criando os dados e inserindo o pont no banco de dados

--- endpoints/photo_post.php
+++ endpoints/photo_post.php
@@ -15,12 +15,40 @@ function api_photo_post($request) {
   $nome = sanitize_text_field($request["nome"]);
   $peso = sanitize_text_field($request["peso"]);
   $idade = sanitize_text_field($request["idade"]);
+  $files = $request->get_file_params();
 
   // Testa se algum dos campos foi vazio.
-  if (empty($nome) || empty($peso) || empty($idade)) {
+  if (empty($nome) || empty($peso) || empty($idade) || empty($files)) {
       $response = new WP_Error("error", "Dados incompletos", ["status" => 422]);
       return rest_ensure_response($response);
   }
+
+    // Cria os dados para serem passados na função que cria o post.
+    $response = [
+      "post_author" => $user_id,
+      "post_type" => "post",
+      "post_status" => "publish",
+      "post_title" => $nome,
+      "post_content" => $nome,
+      "files" => $files,
+      "meta_input" => [
+          "peso" => $peso,
+          "idade" => $idade,
+          "acessos" => 0
+      ]
+  ];
+
+  // Cria e insere o post no banco de dados.
+  $post_id = wp_insert_post($response);
+
+    // Requisição media_handle_upload.
+    require_once ABSPATH."wp-admin/includes/image.php";
+    require_once ABSPATH."wp-admin/includes/file.php";
+    require_once ABSPATH."wp-admin/includes/media.php";
+
+    $photo_id = media_handle_upload("img", $post_id);
+    update_post_meta($post_id, "img", $photo_id);
+
   return rest_ensure_response($response);
 }
 
