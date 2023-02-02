38bfcd1e5ababf52675eb5d74270958bfea53d75
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-02 15:02:26 -0300

    verificando usuario valido

--- endpoints/photo_post.php
+++ endpoints/photo_post.php
@@ -1,6 +1,14 @@
 <?php
   function api_photo_post($request){
+     // Busca os dados do usuario que está logado.
     $user = wp_get_current_user();
+    $user_id = $user->ID;
+
+    // Testa se o usuário passado foi inválido.
+    if ($user_id === 0) {
+      $response = new WP_Error("error", "Usuário não possui permissão.", [
+          "status" => 401
+      ]);
 
     return rest_ensure_response(3);
   } 
