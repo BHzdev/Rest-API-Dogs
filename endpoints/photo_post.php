983db2a905d45fc291bed754704814e3177389cf
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-02 15:10:51 -0300

    pegando os dados do post, e verificando se algum campo foi enviado vazio

--- endpoints/photo_post.php
+++ endpoints/photo_post.php
@@ -1,24 +1,36 @@
 <?php
-  function api_photo_post($request){
-     // Busca os dados do usuario que está logado.
-    $user = wp_get_current_user();
-    $user_id = $user->ID;
+function api_photo_post($request) {
+  // Busca os dados do usuario que está logado.
+  $user = wp_get_current_user();
+  $user_id = $user->ID;
 
-    // Testa se o usuário passado foi inválido.
-    if ($user_id === 0) {
+  // Testa se o usuário passado foi inválido.
+  if ($user_id === 0) {
       $response = new WP_Error("error", "Usuário não possui permissão.", [
           "status" => 401
       ]);
-
-    return rest_ensure_response(3);
-  } 
-
-  // Função para registrar o endpoint da API.
-  function register_api_photo_post(){
-    register_rest_route('api', '/photo', [
-      'methods' => WP_REST_Server::CREATABLE,
-      'callback' => 'api_photo_post',
-    ]);
+      return rest_ensure_response($response);
   }
-  add_action('rest_api_init', 'register_api_photo_post');
+
+  $nome = sanitize_text_field($request["nome"]);
+  $peso = sanitize_text_field($request["peso"]);
+  $idade = sanitize_text_field($request["idade"]);
+
+  // Testa se algum dos campos foi vazio.
+  if (empty($nome) || empty($peso) || empty($idade)) {
+      $response = new WP_Error("error", "Dados incompletos", ["status" => 422]);
+      return rest_ensure_response($response);
+  }
+  return rest_ensure_response($response);
+}
+
+// Função para registrar o endpoint da API.
+function register_api_photo_post() {
+  register_rest_route('api', '/photo', [
+      'methods' => WP_REST_Server::CREATABLE,
+      'callback' => 'api_photo_post'
+  ]);
+}
+
+add_action('rest_api_init', 'register_api_photo_post');
 ?>
