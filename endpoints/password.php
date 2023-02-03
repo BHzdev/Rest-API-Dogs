8b55037406517aec08dce6a00a5159c0492df67d
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-03 17:44:05 -0300

    feat: Validação login usúario

--- endpoints/password.php
+++ endpoints/password.php
@@ -3,12 +3,27 @@ function api_password_lost($request) {
   $login = $request['login'];
   $url = $request['url'];
 
-    // Verifica se o login está vazio.
-    if (empty($login)) {
-      $reponse = new WP_Erro("error", "Informe o email ou login.", [
-          "status" => 406
-      ]);
+  // Verifica se o login está vazio.
+  if (empty($login)) {
+    $reponse = new WP_Erro("error", "Informe o email ou login.", [
+        "status" => 406
+    ]);
       return rest_ensure_response($response);
+  }
+  // Salva o login do usuário em uma variavel
+  $user = get_user_by('email', $login);
+
+  // Validação para que o usuario consiga entrar tanto com o email quando seu nome de usuario(login)
+  if(empty($user)){
+    $user = get_user_by('login', $login)
+  }
+
+  // Verifica se o usuário existe.
+  if (empty($login)) {
+    $reponse = new WP_Erro("error", "Usuário não existe.", [
+        "status" => 401
+    ]);
+    return rest_ensure_response($response);
   }
   
   return rest_ensure_response($request);
