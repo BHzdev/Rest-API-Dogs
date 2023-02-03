3e8bae55150e0d1b15cd5e2774259219d6a261c1
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-03 13:07:40 -0300

    fix photo_ger

--- endpoints/photo_get.php
+++ endpoints/photo_get.php
@@ -1,22 +1,18 @@
 <?php
-function api_comment_get($request) {
+function api_photo_get($request) {
   $post_id = $request["id"];
+  $post = get_post($post_id);
 
-  // Puxa todos os comentários do post.
-  $comments = get_comments([
-      "post_id" => $post_id
-  ]);
-
-  return rest_ensure_response($comments);
+  return rest_ensure_response($post_id);
 }
 
 // Função para registrar o endpoint da API.
-function register_api_comment_get() {
-  register_rest_route('api', '/comment/(?P<id>[0-9]+)', [
+function register_api_photo_get() {
+  register_rest_route('api', '/photo/(?P<id>[0-9]+)', [
       'methods' => WP_REST_Server::READABLE,
-      'callback' => 'api_comment_get'
+      'callback' => 'api_photo_get'
   ]);
 }
 
-add_action('rest_api_init', 'register_api_comment_get');
+add_action('rest_api_init', 'register_api_photo_get');
 ?>
