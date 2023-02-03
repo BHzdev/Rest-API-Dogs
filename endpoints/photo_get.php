e7a01de507217aa4ca8c9030ed5e84a06dbadb9c
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-03 13:10:12 -0300

    validação básica, verifica se o post solicitado existe

--- endpoints/photo_get.php
+++ endpoints/photo_get.php
@@ -3,7 +3,15 @@ function api_photo_get($request) {
   $post_id = $request["id"];
   $post = get_post($post_id);
 
-  return rest_ensure_response($post_id);
+  //Verifica se o post solicitado existe.
+  if(!isset($post) || empty($post_id)){
+    $response = new WP_Erro("error", "Post não encontrado.", [
+      "status" => 404
+  ]);
+  return rest_ensure_response($response);
+  }
+
+  return rest_ensure_response($post);
 }
 
 // Função para registrar o endpoint da API.
